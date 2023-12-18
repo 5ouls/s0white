d0374a5f71667adaad5db3ad5ebba854f20fea73
5ouls <150913550+5ouls@users.noreply.github.com>
2023-12-18 15:09:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ app_id = "wxbf156391db1a7d84"
 # 公众号appSecret
 app_secret = "082d36161b055a085d8b164763b2adaa"
 # 模板消息id
-template_id = "LGprJCtGlSPydCEQ6k1otFZm6RZFmkkUhlZHkRq6dXg"
+template_id = "Y8eRr3kiAZ_YvseqTcZhr9HDKBzpcLF7iJzLSLS14hQ"
 
 # 这是openid
 user = ["o1sO76K1c0RkYBbMP9-YwRFB11aw", "o1sO76BFyc0Tq1Nlp4cM02lz5KTo"]
